9881d1f75848e786bd6b5e5802a4bb04ec9bf404
Johannes <johannes@jitesoft.com>
2018-03-19 11:45:57 +0100

    Removed old containers from docker-compose file, removed old legacy code.

--- resources/lang/vendor/cookieConsent/en/texts.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'message' => '<p>We use cookies to personalise content and analyse our trafic.
-We also share information about your use of the site with our analytics partner (google) who may combine
-it with other information you\'ve provided to them or they have collected from your use of their services.</p>',
-    'agree' => 'Dismiss',
-];
--- resources/views/admin/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.layout-admin')
-@section('content')
-    <div class="content">
-        <h1>
-            Admin Dashboard.
-        </h1>
-    </div>
-@endsection
